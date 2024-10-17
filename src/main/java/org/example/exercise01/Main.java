e5df78b65447b1f294c22c49aaeba6290a32e930
Catalin Barsan <bcn@catalinbarsan.es>
2024-10-17 10:06:56 +0200

    Change the arg. of NoGenericMethods to implement no generic arg.

--- src/main/java/org/example/exercise01/Main.java
+++ src/main/java/org/example/exercise01/Main.java
@@ -3,12 +3,10 @@ package org.example.exercise01;
 public class Main {
     // Main method to test
     public static void main(String[] args) {
-        // Creating objects with different types and positions
-        NoGenericMethods<Integer, String, Double> obj1 = new NoGenericMethods<>(1, "Hello", 3.14);
-        NoGenericMethods<String, Integer, Character> obj2 = new NoGenericMethods<>("World", 42, 'A');
+        // Creating an object with specific types
+        NoGenericMethods obj = new NoGenericMethods(1, "Hello", 3.14);
 
         // Printing out the arguments
-        System.out.println("Object 1: " + obj1.getArg1() + ", " + obj1.getArg2() + ", " + obj1.getArg3());
-        System.out.println("Object 2: " + obj2.getArg1() + ", " + obj2.getArg2() + ", " + obj2.getArg3());
+        System.out.println("Object: " + obj.getArg1() + ", " + obj.getArg2() + ", " + obj.getArg3());
     }
 }
--- src/main/java/org/example/exercise01/NoGenericMethods.java
+++ src/main/java/org/example/exercise01/NoGenericMethods.java
@@ -1,41 +1,42 @@
 package org.example.exercise01;
 
-public class NoGenericMethods<T1, T2, T3> {
-    private T1 arg1;
-    private T2 arg2;
-    private T3 arg3;
+public class NoGenericMethods {
+    private Integer arg1;
+    private String arg2;
+    private Double arg3;
 
     // Constructor
-    public NoGenericMethods(T1 arg1, T2 arg2, T3 arg3) {
+    public NoGenericMethods(Integer arg1, String arg2, Double arg3) {
         this.arg1 = arg1;
         this.arg2 = arg2;
         this.arg3 = arg3;
     }
 
-    // Getters and Setters
-    public T1 getArg1() {
+    // Getters and Setters for specific types
+    public Integer getArg1() {
         return arg1;
     }
 
-    public void setArg1(T1 arg1) {
+    public void setArg1(Integer arg1) {
         this.arg1 = arg1;
     }
 
-    public T2 getArg2() {
+    public String getArg2() {
         return arg2;
     }
 
-    public void setArg2(T2 arg2) {
+    public void setArg2(String arg2) {
         this.arg2 = arg2;
     }
 
-    public T3 getArg3() {
+    public Double getArg3() {
         return arg3;
     }
 
-    public void setArg3(T3 arg3) {
+    public void setArg3(Double arg3) {
         this.arg3 = arg3;
     }
 }
+
 
 
